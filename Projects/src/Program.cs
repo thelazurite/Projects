6810ab2594d3183ff11d56e321fa8dae8767f2b3
Dylan Eddies <dylan.eddies.1996@outlook.com>
2017-02-27 02:00:07 +0000

    Minor Exception Handling changes

--- Projects/src/Program.cs
+++ Projects/src/Program.cs
@@ -18,63 +18,70 @@ namespace Projects
         [STAThread]
         public static void Main(string[] args)
         {
+            // if the user is not running a Unix Based OS - set the enviroment var path accordingly
+            if (!PrjHandler.IsUnix)
+                Environment.SetEnvironmentVariable("PATH",
+                    Environment.Is64BitOperatingSystem
+                        ? @"C:\msys64\mingw64\bin"
+                        : @"C:\msys32\mingw32\bin"
+                    );
             try
             {
-                if (!PrjHandler.IsUnix)
-                    Environment.SetEnvironmentVariable("PATH",
-                        Environment.Is64BitOperatingSystem
-                            ? @"C:\msys64\mingw64\bin"
-                            : @"C:\msys32\mingw32\bin"
-                        );
-
                 Application.Init();
+            }
+            catch (DllNotFoundException e)
+            {
+                var str = $"A file required by Projects is missing:\n\n{e.Message}.";
 
-                // store system's default font in variable
-                var font = SystemFonts.DefaultFont;
-                // set the font to the default font
-                Settings.Default.FontName = font.Name + " " + font.SizeInPoints;
-                // force usage of smooth font
-                Settings.Default.XftAntialias = 1;
-                Settings.Default.XftRgba = "rgb";
+                if (!PrjHandler.IsUnix)
+                    MessageBox.Show(str, "Missing prerequisite", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else Console.WriteLine(str);
 
-                // if arguments have been passed to the program
-                if (args.Length != 0)
-                {
-                    // store first argument
-                    var file = args[0];
+                Environment.Exit(0);
+            }
+            catch (TypeInitializationException e)
+            {
+                var str = $"An issue occurred while trying to load projects:\n\n{e.Message}\n\nIs Gtk installed?";
 
-                    // get the file's extension 
-                    var extension = Path.GetExtension(file);
-                    //Console.WriteLine(extension);
+                if (!PrjHandler.IsUnix)
+                    MessageBox.Show(str, "Check Install", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else Console.WriteLine(str);
 
-                    // if the extension isn't null, the file exists and is of the right extension, and the lock file
-                    // does not exist,
-                    // load the main application with the argument passed to the program
-                    if (extension != null && File.Exists(file) && extension.Equals(".prj") && !File.Exists(file + ".lk"))
-                        new ProjectWindow(file).Show();
-                    // if the requirements are not met, then proceed with the startup process
-                    else
-                        Startup();
-                    Application.Run();
-                }
-                // if no arguments have been passed then go to the startup process
+                Environment.Exit(0);
+            }
+
+            // store system's default font in variable
+            var font = SystemFonts.DefaultFont;
+            // set the font to the default font
+            Settings.Default.FontName = font.Name + " " + font.SizeInPoints;
+            // force usage of smooth font
+            Settings.Default.XftAntialias = 1;
+            Settings.Default.XftRgba = "rgb";
+
+            // if arguments have been passed to the program
+            if (args.Length != 0)
+            {
+                // store first argument
+                var file = args[0];
+
+                // get the file's extension 
+                var extension = Path.GetExtension(file);
+                //Console.WriteLine(extension);
+
+                // if the extension isn't null, the file exists and is of the right extension, and the lock file
+                // does not exist,
+                // load the main application with the argument passed to the program
+                if (extension != null && File.Exists(file) && extension.Equals(".prj") && !File.Exists(file + ".lk"))
+                    new ProjectWindow(file).Show();
+                // if the requirements are not met, then proceed with the startup process
                 else
                     Startup();
+                Application.Run();
             }
-            catch (DllNotFoundException dnfe)
-            {
-                if (!dnfe.Message.EndsWith(".dll"))
-                {
-                    // fall back to winforms messagebox if gtk dependency is missing 
-                    MessageBox.Show($"A file required by Projects is missing:\n\n{dnfe.Message} .",
-                        "Missing prerequisite", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                else if (dnfe.Message.EndsWith(".dll"))
-                {
-                    MessageBox.Show($"A DLL file required by Projects is missing: {dnfe.Message}.",
-                        "Missing library", MessageBoxButtons.OK, MessageBoxIcon.Error); //fall back to winforms
-                }
-            }
+            // if no arguments have been passed then go to the startup process
+            else
+                Startup();
+
         }
 
         /// <summary>
