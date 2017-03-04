b9e06e890fd2618cab8293461fb2ce8f00db6cc9
Dylan Eddies <dylan.eddies.1996@outlook.com>
2017-03-04 16:15:51 +0000

    Implementing Unix file handling

--- Projects/src/main/backend/PrjHandler.cs
+++ Projects/src/main/backend/PrjHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using GLib;
 using Gtk;
 using Settings = Projects.Properties.Settings;
 
@@ -25,70 +27,105 @@ namespace Projects.main.backend
         public static string PathToLock;
         private static StreamWriter _lock;
 
+        [DllImport("libgtk-3-0.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool gtk_file_chooser_set_current_folder(IntPtr raw, IntPtr filename);
+        public static bool SetCurrentFolder(string filename, IntPtr handle)
+        {
+            var num = Marshaller.StringToFilenamePtr(filename);
+            var flag = gtk_file_chooser_set_current_folder(handle, num);
+            Marshaller.Free(num);
+            return flag;
+        }
+
         public static bool Open(Widget parent)
         {
-            using (var openDialog = new OpenFileDialog())
+            var file = string.Empty;
+            if(!IsUnix)
+                using (var openDialog = new OpenFileDialog())
+                {
+                    // check file exists
+                    openDialog.CheckFileExists = true;
+                    // If the previously browsed directory exists, then direct the user to it, otherwise direct to home directory.
+                    openDialog.InitialDirectory = Directory.Exists(Settings.Default.PreviousBrowseFolder)
+                        ? Settings.Default.PreviousBrowseFolder
+                        : Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    // Set the filter to show only 'prf' files
+                    openDialog.Filter = "Projects file (*.prj)|*.prj|Lock file (*.prj.lk) |*.prj.lk|All Files (*.*)|*.*";
+                    openDialog.FilterIndex = 1;
+                    //RestoreDirectory = true,
+                    openDialog.ShowReadOnly = false;
+                    openDialog.ShowDialog();
+
+                    file = openDialog.FileName;
+                    openDialog.Reset();
+                }
+            else
             {
-                // check file exists
-                openDialog.CheckFileExists = true;
-                // If the previously browsed directory exists, then direct the user to it, otherwise direct to home directory.
-                openDialog.InitialDirectory = Directory.Exists(Settings.Default.PreviousBrowseFolder)
-                    ? Settings.Default.PreviousBrowseFolder
-                    : Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                // Set the filter to show only 'prf' files
-                openDialog.Filter = "Projects file (*.prj)|*.prj|Lock file (*.prj.lk) |*.prj.lk|All Files (*.*)|*.*";
-                openDialog.FilterIndex = 1;
-                //RestoreDirectory = true,
-                openDialog.ShowReadOnly = false;
-                openDialog.ShowDialog();
-
-                var file = openDialog.FileName;
-
-                openDialog.Reset();
-
-                // if no file has been provided / the file does not exist - do not continue
-                if (string.IsNullOrEmpty(file) || !File.Exists(file)) return false;
-                //Console.WriteLine(Path.GetExtension(file));
-
-                // ensure that the correct file extension is being used
-                if (Path.GetExtension(file) != ".prj")
+                using (var openDialog = new FileChooserDialog("Open File", parent as Window, FileChooserAction.Open))
                 {
-                    using (var md = new MessageDialog(parent as Window, DialogFlags.Modal, MessageType.Error,
-                        ButtonsType.Close, "The selected file is not recognized by Projects."))
+                    using (var openFilter = new FileFilter())
                     {
-                        md.Run();
-                        md.Destroy();
+                        openDialog.AddButton("Open", ResponseType.Ok);
+                        openDialog.AddButton("Cancel", ResponseType.Close);
+                        SetCurrentFolder(Directory.Exists(Settings.Default.PreviousBrowseFolder)
+                        ? Settings.Default.PreviousBrowseFolder
+                        : Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),openDialog.Handle);
+
+                        openFilter.AddMimeType("Projects File");
+                        openFilter.AddPattern("*.prj");
+
+                        openDialog.Filter = openFilter;
+                        if (openDialog.Run() == (int) ResponseType.Ok)
+                            file = openDialog.File.ParsedName;
+                        openDialog.Destroy();
                     }
-                    return false;
                 }
-                // get the path from the selected file, by finding the last occurrence of a directory separator and save it into 
-                // the "PreviousBrowseFolder" Setting
-                Settings.Default.PreviousBrowseFolder = file.Substring(0,
-                    file.LastIndexOf(Path.DirectorySeparatorChar));
-
-                // save settings
-                Settings.Default.Save();
-
-                if (File.Exists(file + ".lk"))
-                {
-                    using (
-                        var dialog = new MessageDialog(parent as Window, DialogFlags.DestroyWithParent,
-                            MessageType.Error,
-                            ButtonsType.Ok,
-                            $"The file is currently in use. If you're sure that this isn't the case, please delete the following file:\n {file}.lk")
-                        )
-                    {
-                        dialog.Run();
-                        dialog.Destroy();
-                    }
-                    return false;
-                }
-
-                var window = new ProjectWindow(file);
-                window.Show();
-                parent.Destroy();
-                return true;
             }
+
+            Console.WriteLine(file);
+            // if no file has been provided / the file does not exist - do not continue
+            if (string.IsNullOrEmpty(file) || !File.Exists(file)) return false;
+            //Console.WriteLine(Path.GetExtension(file));
+
+            // ensure that the correct file extension is being used
+            if (Path.GetExtension(file) != ".prj")
+            {
+                using (var md = new MessageDialog(parent as Window, DialogFlags.Modal, MessageType.Error,
+                    ButtonsType.Close, "The selected file is not recognized by Projects."))
+                {
+                    md.Run();
+                    md.Destroy();
+                }
+                return false;
+            }
+            // get the path from the selected file, by finding the last occurrence of a directory separator and save it into 
+            // the "PreviousBrowseFolder" Setting
+            Settings.Default.PreviousBrowseFolder = file.Substring(0,
+                file.LastIndexOf(Path.DirectorySeparatorChar));
+
+            // save settings
+            Settings.Default.Save();
+
+            if (File.Exists(file + ".lk"))
+            {
+                using (
+                    var dialog = new MessageDialog(parent as Window, DialogFlags.DestroyWithParent,
+                        MessageType.Error,
+                        ButtonsType.Ok,
+                        $"The file is currently in use. If you're sure that this isn't the case, please delete the following file:\n {file}.lk")
+                    )
+                {
+                    dialog.Run();
+                    dialog.Destroy();
+                }
+                return false;
+            }
+
+            var window = new ProjectWindow(file);
+            window.Show();
+            parent.Destroy();
+            return true;
+
         }
 
         // create the lock file
